77549b34f63f8524dc7cc14b2415efdf0c2bfb78
Qs-F <qsf@mail.queensya.com>
2015-11-05 13:11:49 +0900

    alpha complete

--- nurunuru.js
+++ nurunuru.js
@@ -4,22 +4,27 @@
 document.addEventListener("DOMContentLoaded", function() {
 	var testElem = document.getElementsByClassName("firable")[0];
 	var firable = false;
-
+	var hasTapEvent = (function(){
+		var div = document.createElement('div');
+		div.setAttribute('ontouchstart', 'return');
+		return (typeof div.ontouchstart === 'function');
+	})();
 	testElem.addEventListener("nurunuruEvent", function() {
 		testElem.classList.toggle("nu-active");
 	}, false);
-	
-	testElem.addEventListener("click", function() {
-		var e = document.createEvent("UIEvents");
-		e.initEvent("nurunuruEvent", true, false);
-		this.dispatchEvent(e);
-	}, false);
-
-	testElem.addEventListener("touchstart", function() {
-		testElem.addEventListener("touchend", function() {
+	if(hasTapEvent == false) {
+		testElem.addEventListener("click", function() {
 			var e = document.createEvent("UIEvents");
 			e.initEvent("nurunuruEvent", true, false);
 			this.dispatchEvent(e);
 		}, false);
-	}, false);
+	} else {
+		testElem.addEventListener("touchstart", function() {
+			testElem.addEventListener("touchend", function() {
+				var e = document.createEvent("UIEvents");
+				e.initEvent("nurunuruEvent", true, false);
+				this.dispatchEvent(e);
+			}, false);
+		}, false);
+	}
 }, false);
